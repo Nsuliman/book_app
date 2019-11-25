057a467a400eb7e96805a741216cd3c4f56dfe18
Nsuliman <nsuliman88@gmail.com>
2019-11-25 13:55:51 +0200

    3rd  feature Done

--- server.js
+++ server.js
@@ -42,17 +42,42 @@ server.get('/test', (req,res) => {
 /************************* Test linked HTML file ********************* */
 server.get('/incoming', (req,res) => {
     console.log('Got here from a get request ... ', req.query);
-    res.redirect('/thanks.html');
+    res.redirect('/dataBook.html');
   });
   server.post('/incoming', (req,res) => {
     console.log('Got here from a post request ... ', req.body);
-    res.redirect('/thanks.html');
+    res.redirect('/dataBook.html');
   });
 
 
+  server.get('/books', (req,res) => {
+    let url = 'https://www.googleapis.com/books/v1/volumes?q=books';
+    // console.log(' \n\n\n urllllllllllllllllllllllllllllll : ', url);
+     superagent.get(url)
+      .then( data => {
+        console.log(' \n\n\n dataaaaaaaaaaaaaaaaaaaaaaaaaaaaa Boddddddddddddddy : ', data.body);
+        console.log('\n\n\n dataaaaaaaaaaaaaaaaaaaaaaaaaaaaa Itemmmmmmmmmmmmmmmms : ', data.body.items);
+        // const book = data.body;
+        res.render('pages/searches/show' , {books:data.body.items});
+        return data.body.items.map( c =>
+        {
+            // return new Book (c)}); 
+      });
+  });
+      const weather = data.body;
+      return weather.daily.data.map((day) => {
+        return new Weather(day);
+      });
+    });
 
-
-
+  function Book(data) {
+    this.id = data.id;
+    this.url_img = data.volumeInfo.imageLinks.thumbnail;    
+    this.title = data.volumeInfo.title;
+    this.author = data.volumeInfo.authors;
+    this.description = data.volumeInfo.description;
+  
+  } // End of location constructor function 
 
 
 
