6f1e9f89f324aba758769861ad0e0e0143935ac6
Nsuliman <nsuliman88@gmail.com>
2019-11-26 21:57:50 +0200

    LAB11 DONE

--- server.js
+++ server.js
@@ -17,71 +17,81 @@ const server = express();
 server.use(cors());
 
 server.use(express.json());
-server.use(express.urlencoded({ extended: true }));
 server.use(express.static('./public'));
+server.use(express.urlencoded({ extended: true }));
 server.set('view engine', 'ejs');
 
 /*********************************************************************/
 
 /*************** Test Route *****************/
-server.get('/test', (req, res) => {
-    res.status(200).send('Hello Ya Tester ');
-});
+// server.get('/test', (req, res) => {
+//     res.status(200).send('Hello Ya Tester ');
+// });
 
-/**************** Render index **************/
-server.get('/main', (req, res) => {
+// /**************** Render index **************/
+// server.get('/main', (req, res) => {
+//     res.render('pages/index');
+// });
+
+// /**************** Re-direct index **************/
+// server.get('/main', (req, res) => {
+//     console.log('Got here from a get request ... ', req.query);
+//     res.redirect('/index.html');
+// });
+
+// /************************* Test linked HTML file ********************* */
+// server.get('/incoming', (req, res) => {
+//     console.log('Got here from a get request ... ', req.query);
+//     res.redirect('/dataBook.html');
+// });
+// server.post('/incoming', (req, res) => {
+//     console.log('Got here from a post request ... ', req.body);
+//     res.redirect('/dataBook.html');
+// });
+
+/*************************************** */
+
+server.get('/searches' , (req,res) =>
+{
     res.render('pages/index');
 });
 
-/**************** Re-direct index **************/
-server.get('/main', (req, res) => {
-    console.log('Got here from a get request ... ', req.query);
-    res.redirect('/index.html');
-});
+server.post('/searches', (req, res) =>{
+    let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+        // console.log('urllllllllllllllllllllllllllllll : ', url);
 
-/************************* Test linked HTML file ********************* */
-server.get('/incoming', (req, res) => {
-    console.log('Got here from a get request ... ', req.query);
-    res.redirect('/dataBook.html');
-});
-server.post('/incoming', (req, res) => {
-    console.log('Got here from a post request ... ', req.body);
-    res.redirect('/dataBook.html');
-});
+        if (req.body.searchtype === 'title') {
+            url = url + req.body.search;
+        }
+        else if (req.body.searchtype === 'author') {
+            url = url + req.body.search;
+            // console.log(url)
+        }
 
-
-server.get('/books', (req, res) =>{
-    let url = 'https://www.googleapis.com/books/v1/volumes?q=books';
-    // console.log('urllllllllllllllllllllllllllllll : ', url);
     superagent.get(url)
         .then(data => {
-            // // console.log('dataaaaaaaaaaaaaaaaaaaaaaaaaaaaa Boddddddddddddddy : ', data.body);
-            // // console.log('dataaaaaaaaaaaaaaaaaaaaaaaaaaaaa Itemmmmmmmmmmmmmmmms : ', data.body.items);
-
-            // const book = data.body;
-            // return book.daily.data.map((day) => {
-            //     return new Book(day);
-            // });
-            res.render('pages/searches/show', { books:data.body.items })
-
+            // console.log('dataaaaaaaaaaaaaaaaaaaaaaaaaaaaa Boddddddddddddddy : ', data.body);
+            // console.log('dataaaaaaaaaaaaaaaaaaaaaaaaaaaaa Itemmmmmmmmmmmmmmmms : ', data.body.items);
+            
+            let book = data.body.items;
+            res.render('pages/searches/show', { books:book })
+            // return book.items.map((rest) => {
+            //     console.log(' rest : ', rest);
+                //  new Book(book);
+            });
     });
-});
+    
 
-    // return superagent.get(url)
-    //     .then(data => {
-    //         const weather = data.body;
-    //         return weather.daily.data.map((day) => {
-    //             return new Weather(day);
-    //         });
-    //     });
 
-    function Book(data) {
-        this.id = city;
-        this.formatted_query = data.results[0].formatted_address;
-        this.latitude = data.results[0].geometry.location.lat;
-        this.longitude = data.results[0].geometry.location.lng;
 
-    } // End of location constructor function 
+    // function Book(data) {
+    //     this.title = data.volumeInfo.title;
+    //     this.author = data.volumeInfo.authors && data.volumeInfo || '';
+    //     this.ISBN = data.volumeInfo.industryIdentifiers.type && data.volumeInfo.industryIdentifiers || '';
+    //     this.image = data.imageLinks.thumbnail && data.imageLinks || '';
+    //     this.description = data.volumeInfo.description && data.volumeInfo || '';
+
+    // } // End of location constructor function 
 
 
 
