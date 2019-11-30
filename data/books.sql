9a9703ad157c6630c4bcba4b6513dac0a813c4a9
Nsuliman <nsuliman88@gmail.com>
2019-11-30 18:07:32 +0200

    add sql

--- data/books.sql
+++ data/books.sql
@@ -5,8 +5,8 @@ image_url VARCHAR(255),
   title VARCHAR(255),
   author VARCHAR(255),
     booktype VARCHAR(255),
-  descrp TEXT,
+  descrp TEXT
 );
 
-INSERT INTO booksql (title, author, image_url, descrp, booktype)
+INSERT INTO booksql (image_url,title, author, booktype, descrp )
 VALUES('My Father','Nawal','image','Lovely dad','1961');
