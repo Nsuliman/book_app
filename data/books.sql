1f65783e2b2b830f22852db5af513dd98a0e128d
Nsuliman <nsuliman88@gmail.com>
2019-11-30 17:56:24 +0200

    add sql

--- data/books.sql
+++ data/books.sql
@@ -1,9 +1,12 @@
-DROP TABLE IF EXISTS booksTab;
-CREATE TABLE booksTab (
+DROP TABLE IF EXISTS booksql;
+CREATE TABLE booksql  (
   id SERIAL PRIMARY KEY,
+image_url VARCHAR(255),
   title VARCHAR(255),
   author VARCHAR(255),
-  description TEXT,
-  booktype VARCHAR(255),
-  bookshelf VARCHAR(255),
+    booktype VARCHAR(255),
+  descrp TEXT,
 );
+
+INSERT INTO booksql (title, author, image_url, descrp, booktype)
+VALUES('My Father','Nawal','image','Lovely dad','1961');
